229822a0f875b2319c077f329eb4e7f37589eb2c
wa23991 <wenyi.an@yardi.com>
2019-05-25 20:02:43 -0600

    Added tests for calculateTotalScore.

--- src/main/java/application/BowlingService.java
+++ src/main/java/application/BowlingService.java
@@ -1,4 +1,14 @@
 package application;
 
+import domain.Frame;
+
+import java.util.List;
+
 public class BowlingService {
+    public BowlingService() {
+    }
+
+    public List<Frame> caculateTotalScore(List<Frame> frames) {
+        return null;
+    }
 }
--- src/test/java/application/BowlingServiceTest.java
+++ src/test/java/application/BowlingServiceTest.java
@@ -1,5 +1,29 @@
+package application;
+
+import domain.Frame;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class BowlingServiceTest {
+public class BowlingServiceTest {
 
+    private BowlingService bowlingService;
+
+    @Before
+    public void setUp() {
+        bowlingService = new BowlingService();
+    }
+
+    @Test
+    public void caculateTotalScore_shouldReturnEmptyFrames_whenTheGameHasNoFrames(){
+        List<Frame> frames = new ArrayList<>();
+
+        List<Frame> actual = bowlingService.caculateTotalScore(frames);
+
+        assertEquals(actual.size(), 0);
+    }
 }
